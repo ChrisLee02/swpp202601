b6eabf2f4e5917aaef3977f1e94d76967ca7235f
Chrislee02 <hdchris765@snu.ac.kr>
2026-03-29 23:07:46 +0900

    assign2 ongoing

--- assignments/assignment2/impl/maybe_something.cpp
+++ assignments/assignment2/impl/maybe_something.cpp
@@ -1,1 +1,72 @@
 // Implement module maybe_something here
+module;
+#include <string>
+#include <string_view>
+
+module maybe_something;
+
+namespace maybe_something {
+
+// Static member initialization
+size_t Something::global_num_objects = 0;
+size_t Nothing::global_num_objects = 0;
+
+// Something constructors
+Something::Something(const std::string &s) : state(State::VALID) {
+  global_num_objects++;
+  content = s;
+}
+
+Something::Something(std::string &&s)
+    : content(std::move(s)), state(State::VALID) {
+  global_num_objects++;
+}
+
+Something::Something(const Something &other) {
+  global_num_objects++;
+  content = other.content;
+  state = other.state;
+}
+
+Something &Something::operator=(const Something &other) {
+  content = other.content;
+  return *this;
+}
+
+Something::Something(Something &&other) noexcept {}
+
+Something &Something::operator=(Something &&other) noexcept {}
+
+Something::~Something() {}
+
+std::string_view Something::view_content() const {}
+
+size_t Something::count() const noexcept {}
+
+// Nothing constructors
+Nothing::Nothing() {}
+
+Nothing::Nothing(const Nothing &other) {}
+
+Nothing &Nothing::operator=(const Nothing &other) {}
+
+Nothing::Nothing(Nothing &&other) noexcept {}
+
+Nothing &Nothing::operator=(Nothing &&other) noexcept {}
+
+Nothing::~Nothing() {}
+
+size_t Nothing::count() const noexcept {}
+
+// Export functions
+MaybeSomething create_something_by_copying(const std::string &s) {}
+
+MaybeSomething create_something_by_moving(std::string &&s) {}
+
+MaybeSomething create_nothing() {}
+
+std::string_view view(const MaybeSomething &thing) {}
+
+size_t count_siblings(const MaybeSomething &thing) {}
+
+} // namespace maybe_something
--- assignments/assignment2/interface/collect.cppm
+++ assignments/assignment2/interface/collect.cppm
@@ -9,16 +9,30 @@ namespace collect {
 export template <std::movable T, std::movable E>
 std::expected<std::vector<T>, E>
 collect_expected(const std::vector<std::expected<T, E>> &inputs) {
-  using RetType = std::expected<std::vector<T>, E>;
+  std::vector<T> res;
+  res.reserve(inputs.size());
 
-  return RetType();
-}
+  for (const std::expected<T, E> &item: inputs) {
+    if(!item.has_value()) {
+      return std::unexpected(item.error());
+    }
+    res.push_back(item.value());
+  }
+  return res;
+} 
 
 export template <std::movable T, std::movable E>
 std::expected<std::vector<T>, E>
 move_collect_expected(std::vector<std::expected<T, E>> &&inputs) {
-  using RetType = std::expected<std::vector<T>, E>;
+  std::vector<T> res;
+  res.reserve(inputs.size());
 
-  return RetType();
+  for (std::expected<T, E> &item: inputs) {
+    if(!item.has_value()) {
+      return std::unexpected(std::move(item).error());
+    }
+    res.push_back(std::move(item).value());
+  }
+  return res;
 }
 } // namespace collect
